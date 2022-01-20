739a8587f4838959c1f12c4ecb51e6d056591643
Samuel Granvik <samuelgranvik@Samuels-MacBook-Pro.local>
2022-01-20 15:35:03 +0200

    Reworking the popup.js and background.js to work with the alarms instead of setIntervals. Creating console.logs to better debug the code

--- background.js
+++ background.js
@@ -1,6 +1,7 @@
 // Run each function based on the data.action string gathered frm the clicked btns on the popup
-function popupMsgReceived(data) {
-    switch(data.action) {
+function popupMsgReceived(btn) {
+    console.info('background received ', btn);
+    switch(btn.action) {
         case 'start':
             handleStart();
             break;
@@ -13,54 +14,29 @@ function popupMsgReceived(data) {
     }
 }
 
-/* Function to send back data to the popup.js script
+// Function to send back data to the popup.js script
 function notifyPopupPage(req, sender, sendRes) {
     sendRes({
-        response: "Response from background script"
+        response: 'Response from background script'
     });
-}*/
-
-//browser.runtime.onMessage.addListener(notifyPopupPage);
-browser.runtime.onMessage.addListener(popupMsgReceived);
-
-// Set the initial clock values
-let hr  = 0;
-let min = 0;
-let sec = 0;
+}
 
 /*
- * The functions used in the earlier switch statement - 
- * that handles the timer, Thanks Wesley Branton https://mzl.la/3IjjbON
+ *  An alarm is created every second that runs the startTimer function
+ *  Thanks Wesley Branton https://mzl.la/3IjjbON
 */
 function handleStart() {
-    //timerStartedAt();
-    browser.alarms.onAlarm.addListener(timerStartedAt);
-    browser.alarms.create('timerStartedAt', {
-        periodInMinutes: 1
+    browser.alarms.onAlarm.addListener(startTimer);
+    browser.alarms.create('startTimer', {
+        periodInMinutes: 1 / 60
     });
+    console.info('Handler started')
 }
 
+// Stops the loop for the startTimer alarm
 function handleStop() {
-    browser.alarms.clearAll();
-}
-
-function handleReset() {
-    browser.alarms.clearAll();
-    hr  = 0;
-    min = 0;
-    sec = 0;
-    timer.innerHTML = '00:00:00';
-    displayDate.innerHTML = ''
-}
-
-// Here we get the current time, that is used to display when the timer was started
-function timerStartedAt() {
-    const date = new Date();
-
-    const hr  = String(date.getHours()).padStart(2, "0");
-    const min = String(date.getMinutes()).padStart(2, "0");
-
-    displayDate.innerHTML = 'Timer started ' + hr + ':' + min;
+    console.info('Handler/Timer stopped')
+    browser.alarms.clear('startTimer');
 }
 
 /* 
@@ -68,12 +44,19 @@ function timerStartedAt() {
  *  We begin with parsing the clock string values to integers
  *  The logic later handles the sec, min and hours like a digital clock
 */
+
+let hr  = 0;
+let min = 0;
+let sec = 0;
+
 function startTimer() {
     hr  = parseInt(hr);
     min = parseInt(min);
     sec = parseInt(sec);
 
     sec++;
+
+    console.log('Timer (secs) ', sec);
 
     if (sec == 60) {
         min += 1;
@@ -95,6 +78,7 @@ function startTimer() {
     if (hr < 10) {
         hr = '0' + hr;
     }
-
-    timer.innerHTML = hr + ':' + min + ':' + sec;
 }
+
+browser.runtime.onMessage.addListener(popupMsgReceived);
+browser.runtime.onMessage.addListener(notifyPopupPage);
--- popup/popup.js
+++ popup/popup.js
@@ -1,30 +1,62 @@
 // Elements from the HTML popup
 const displayDate = document.getElementById('displayDate');
-const timer     = document.getElementById('timer');
-const startBtn  = document.getElementById('start');
-const stopBtn   = document.getElementById('stop');
-const resetBtn  = document.getElementById('reset');
+const timer       = document.getElementById('timer');
+const startBtn    = document.getElementById('start');
+const stopBtn     = document.getElementById('stop');
+const resetBtn    = document.getElementById('reset');
 
 /*
- *  The functions below handles the communication between the popup and the background script
- *  notifyBackgroundPage() sends the element that was clicked as an object to the background script
+ *  The function below handles the communication between the popup and the background script
+ *  It sends the element that was clicked as an object to the background script, it uses each btn ID to handle the timer
+ *  It later on handles the response, and errors from the background script
 */
-function handleRes(msg) {
-    console.info(`From the background script:  ${msg.response}`);
-}
-
-function handleErr(err) {
-    console.error(`Handle Error: ${err}`);
-}
-
-function notifyBackgroundPage(element) {
+startBtn.addEventListener('click', () => {
+    console.log('Start btn pressed');
     const sending = browser.runtime.sendMessage({
-        action: element.id
+        action: startBtn.id
     });
-    sending.then(handleRes, handleErr);
+    sending.then (
+        (background) => {
+            console.info(`From the background script: ${background.response}`);
+            timerStartedAt();
+        },
+        (error) => {
+            console.error(`Background script error: ${error}`);
+        }
+    );
+});
+
+stopBtn.addEventListener('click', () => {
+    console.info('Stop btn pressed');
+    const sending = browser.runtime.sendMessage({
+        action: stopBtn.id
+    });
+    sending.then (
+        (background) => {
+            console.info(`From the background script: ${background.response}`);
+            timerStartedAt();
+        },
+        (error) => {
+            console.error(`Background script error: ${error}`);
+        }
+    );
+});
+
+resetBtn.addEventListener('click', handleReset);
+
+// Here we get the current time, that is used to display when the timer was started
+function timerStartedAt() {
+    const date = new Date();
+
+    const hr  = String(date.getHours()).padStart(2, '0');
+    const min = String(date.getMinutes()).padStart(2, '0');
+
+    displayDate.innerHTML = 'Timer started ' + hr + ':' + min;
 }
 
-// Here each popup btn press gets passed to the function that notifies the background script with the data
-startBtn.addEventListener('click', notifyBackgroundPage(startBtn));
-stopBtn.addEventListener('click', notifyBackgroundPage(stopBtn));
-resetBtn.addEventListener('click', notifyBackgroundPage(resetBtn));
+// Stops all alarms running, and resets all HTML
+function handleReset() {
+    browser.alarms.clearAll();
+    timer.innerHTML = '00:00:00';
+    displayDate.innerHTML = ''
+}
