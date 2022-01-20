33a6a289cd086b39831e0a79dc5d2857c56f7f61
Samuel Granvik <samuelgranvik@Samuels-MacBook-Pro.local>
2022-01-21 00:11:13 +0200

    New method of calculating time, the use of localStorage and the Date object in the popup.js script, needs improvment still.

--- popup/popup.js
+++ popup/popup.js
@@ -5,53 +5,60 @@ const startBtn    = document.getElementById('start');
 const stopBtn     = document.getElementById('stop');
 const resetBtn    = document.getElementById('reset');
 
-/*
- *  The function below handles the communication between the popup and the background script
- *  It sends the element that was clicked as an object to the background script, it uses each btn ID to handle the timer
- *  It later on handles the response, and errors from the background script
-*/
-
-function handleRes(background) {
-    console.info(`From the background script: ${background.response}`);
-}
-
-function handleErr(error) {
-    console.error(`Background script error: ${error}`);
-}
+let hr  = 0;
+let min = 0;
+let sec = 0;
 
 startBtn.addEventListener('click', () => {
-    console.log('Start btn pressed');
-
-    const sending = browser.runtime.sendMessage({
-        action: startBtn.id
-    });
-    sending.then(handleRes, handleErr)}
-);
-
-stopBtn.addEventListener('click', () => {
-    console.info('Stop btn pressed');
-
-    const sending = browser.runtime.sendMessage({
-        action: stopBtn.id
-    });
-    sending.then(handleRes, handleErr)}
-);
-
-resetBtn.addEventListener('click', handleReset);
-
-// Here we get the current time, that is used to display when the timer was started
-function timerStartedAt() {
     const date = new Date();
-    
+
     const hr  = String(date.getHours()).padStart(2, '0');
     const min = String(date.getMinutes()).padStart(2, '0');
 
     displayDate.innerHTML = 'Timer started ' + hr + ':' + min;
+});
+
+function handleTime() {
+    var storageTime = localStorage.getItem('startTime');
+
+    if (storageTime === null) {
+        localStorage.setItem('startTime', Date.now());
+    } else {
+        hr  = parseInt(hr);
+        min = parseInt(min);
+        sec = parseInt((Date.now() - storageTime) / 1000);
+
+        if (sec == 60) {
+            min += 1;
+            sec = 0;
+        } else if (min == 60) {
+            hr += 1;
+            min = 0;
+            sec = 0;
+        }
+    
+        if (sec < 10) {
+            sec = '0' + sec;
+        } 
+        
+        if (min < 10) {
+            min = '0' + min;
+        } 
+        
+        if (hr < 10) {
+            hr = '0' + hr;
+        }
+
+        timer.innerHTML = hr + ':' + min + ':' + sec;
+    }
 }
 
-// Stops all alarms running, and resets all HTML
-function handleReset() {
-    browser.alarms.clearAll();
+setInterval(handleTime, 1000);
+
+//stopBtn.addEventListener('click', () => {});
+resetBtn.addEventListener('click', () => {
+    clearInterval(handleTime);
+    localStorage.removeItem('startTime');
     timer.innerHTML = '00:00:00';
-    displayDate.innerHTML = ''
-}
+    displayDate.innerHTML = '';
+});
